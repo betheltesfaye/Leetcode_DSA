c3077902af2f92fb626f5897be971d2f232477cc
Bethel Tesfaye <iamlifejunkie@gmail.com>
2026-06-01 13:24:51 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (48.22%) - LeetSync

--- 3312-number-of-changing-keys/number-of-changing-keys.py
+++ 3312-number-of-changing-keys/number-of-changing-keys.py
@@ -1,12 +1,25 @@
+# class Solution:
+#     def countKeyChanges(self, s: str) -> int:
+#         s = s.lower()
+#         changes = 0
+        
+#         # Start from the second character (index 1)
+#         for i in range(1, len(s)):
+#             # If current char is different from previous, it's a key change
+#             if s[i] != s[i-1]:
+#                 changes += 1
+                
+#         return changes
+
 class Solution:
     def countKeyChanges(self, s: str) -> int:
         s = s.lower()
         changes = 0
         
         # Start from the second character (index 1)
-        for i in range(1, len(s)):
+        for i in range(len(s) - 1):
             # If current char is different from previous, it's a key change
-            if s[i] != s[i-1]:
+            if s[i] != s[i+1]:
                 changes += 1
                 
         return changes
