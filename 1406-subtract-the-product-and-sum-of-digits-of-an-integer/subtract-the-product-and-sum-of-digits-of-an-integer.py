e21f0f45f81d08932e3a868022060e75a2907a75
Bethel Tesfaye <iamlifejunkie@gmail.com>
2026-06-05 12:19:02 +0300

    Time: 0 ms (100.00%) | Memory: 19.2 MB (89.37%) - LeetSync

---
+++ 1406-subtract-the-product-and-sum-of-digits-of-an-integer/subtract-the-product-and-sum-of-digits-of-an-integer.py
@@ -0,0 +1,49 @@
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        # digits = []
+        # digitTen = n
+        # productOfDigits = 1 
+        # sumOfDigits = 0
+        # count = 0
+        # if n < 10:
+        #     return 0
+        
+        # while digitTen > 0:
+        #     digitTen //= 10
+        #     count += 1
+        
+        # for i in range(1, count+1):
+        #     digitTen = n - ((n//10**i) * (10**i))
+        #     if digitTen < 10:
+        #         digits.append(digitTen)
+        #     else:
+        #         digits.append(digitTen // (10**(i-1)))
+    
+        # print(digits)
+        digits = []
+        productOfDigits = 1 
+        sumOfDigits = 0
+
+        if n < 10:
+            return 0
+
+        # Extract digits directly using modulo
+        while n > 0:
+            digits.append(n % 10)  # Captures the last digit (even if it is 0)
+            n //= 10              # Shrinks the number down
+
+            # For 705, digits will be [5, 0, 7]
+            # If you need them in original order [7, 0, 5], just reverse it:
+        
+        for j in digits:
+            productOfDigits *= j
+            sumOfDigits += j 
+        
+        print(digits)
+        return productOfDigits - sumOfDigits
+        
+        
+        
+        
+        
+        
