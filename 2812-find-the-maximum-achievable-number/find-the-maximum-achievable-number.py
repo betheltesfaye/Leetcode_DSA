56a7f375f18a2b7d2ef5a8dfc611cca6cce8f9c4
Bethel Tesfaye <iamlifejunkie@gmail.com>
2026-06-12 08:23:35 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (11.82%) - LeetSync

---
+++ 2812-find-the-maximum-achievable-number/find-the-maximum-achievable-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def theMaximumAchievableX(self, num: int, t: int) -> int:
+        maxUp = num
+        maxDown = num
+        x = 0
+        for i in range(t):
+            maxUp += 1
+            maxDown -= 1
+        
+        if (maxUp == (num + t)):
+            return (maxUp + t)
+        elif (maxDown == (num - t)):
+            return maxDown - 1
+            
+        
+        print(f"Num: {num}, MaxUp: {maxUp}, MaxDown: {maxDown}")
+            
