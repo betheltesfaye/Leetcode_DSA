d155c5944a50706da7e93408555bf8be135a146f
Bethel Tesfaye <iamlifejunkie@gmail.com>
2026-06-21 09:53:01 +0300

    Time: 6 ms (34.07%) | Memory: 19.1 MB (97.78%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,24 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        # Base cases: the square root of 0 is 0, and 1 is 1
+        if x < 2:
+            return x
+            
+        # The square root of x (for x >= 2) will always be between 2 and x // 2
+        low = 2
+        high = x // 2
+        ans = 1 # Tracks the closest integer floor found so far
+        
+        while low <= high:
+            mid = (low + high) // 2
+            square = mid * mid
+            
+            if square == x:
+                return mid
+            elif square < x:
+                ans = mid      # mid could be the potential answer, save it
+                low = mid + 1  # Search the right half for a closer answer
+            else:
+                high = mid - 1 # Search the left half
+                
+        return ans 
