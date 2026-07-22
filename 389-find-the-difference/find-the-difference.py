c848f4df5708074eaac308181d756056a3ee4b03
Bethel Tesfaye <iamlifejunkie@gmail.com>
2026-07-22 19:19:46 +0300

    Time: 3 ms (50.81%) | Memory: 19.2 MB (61.68%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        for i in range(len(t)):
+            if (t.count(t[i]) == s.count(t[i])) and (t[i] in s):
+                continue
+            elif t.count(t[i]) != s.count(t[i]):
+                return t[i]
+        
+        return t[i] 
