9f2c78eaa706d7e357218511ad63302779baee61
Bethel Tesfaye <iamlifejunkie@gmail.com>
2026-07-29 17:16:34 +0300

    Time: 47 ms (10.74%) | Memory: 30 MB (95.83%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,14 @@
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         for i in nums:
+#             if nums.count(i) > 1:
+#                 return True
+#         return False
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return True
+        return False
