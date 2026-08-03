e9f262df41f908aac530861cb52e93206b95107b
Bethel Tesfaye <iamlifejunkie@gmail.com>
2026-08-03 19:50:08 +0300

    Time: 39 ms (34.34%) | Memory: 31 MB (51.10%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -1,11 +1,35 @@
+# class Solution:
+#     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+#         arrayN = []
+#         final = []
+#         for i in range(1, len(nums)+1):
+#             arrayN.append(i)
+#         # for i in nums:
+#         #     if i < len(nums) and i > 1:
+#         #         final.append(i)
+
+#         result = [i for i in arrayN if i not in nums]
+
+#         # print(arrayN)
+#         # print(final)  
+#         # print(result)
+
+#         return result
+
+
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        if max(nums) > len(nums):
-            ogArray = list(range(1, max(nums) + 1))
-        else:
-            ogArray = list(range(1, len(nums) + 1))
-            
-        disNums = list(set(ogArray) ^ set(nums))
-
-        return disNums
-    
+        # Step 1: Mark visited numbers by making the value at their index negative
+        for num in nums:
+            # Use abs() because the number might have been flipped negative already
+            index = abs(num) - 1
+            if nums[index] > 0:
+                nums[index] = -nums[index]
+        
+        # Step 2: Any index that is still positive means its corresponding number never appeared
+        result = []
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                result.append(i + 1)
+                
+        return result
