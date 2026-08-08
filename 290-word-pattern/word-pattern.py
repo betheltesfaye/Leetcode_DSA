2707078e528dacf49e62974a56377881806b4914
Bethel Tesfaye <iamlifejunkie@gmail.com>
2026-08-08 20:54:07 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (28.88%) - LeetSync

---
+++ 290-word-pattern/word-pattern.py
@@ -0,0 +1,42 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        patternList = []
+        sWord = ""
+        sWordList = []
+        for i in pattern:
+            patternList.append(i)
+
+        for i in s:
+            if i == " ":
+                sWordList.append(sWord)
+                sWord = ""
+            else:
+                sWord += i
+        
+        sWordList.append(sWord)
+        
+        print(patternList)
+        print(sWordList)
+
+
+        
+        if len(patternList) != len(sWordList):
+            return False  # Length mismatch
+
+        map1, map2 = {}, {}
+
+        for i, j in zip(patternList, sWordList):
+            # Check if item1's mapping conflicts
+            if i in map1 and map1[i] != j:
+                return False
+            # Check if item2's mapping conflicts
+            if j in map2 and map2[j] != i:
+                return False
+
+            map1[i] = j
+            map2[j] = i
+
+        return True  # No mismatch, patterns match perfectly
+
+
+            
