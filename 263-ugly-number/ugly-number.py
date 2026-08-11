42bd851dd636c87ffbb2de3c4a3f28396c7b8c9c
Bethel Tesfaye <iamlifejunkie@gmail.com>
2026-08-11 20:00:55 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (28.72%) - LeetSync

--- 263-ugly-number/ugly-number.py
+++ 263-ugly-number/ugly-number.py
@@ -1,15 +1,15 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
-        # Edge case: non-positive integers are not ugly numbers
+        # A non-positive integer cannot be ugly
         if n <= 0:
             return False
         
-        # Continuously divide by 2, 3, and 5 as long as it's possible
+        # Repeatedly divide n by 2, 3, and 5
         for factor in [2, 3, 5]:
             while n % factor == 0:
                 n //= factor
-        
-        # If the remaining number is 1, it means it only had 2, 3, and 5 as prime factors
+                
+        # If the remaining number is 1, it's an ugly number
         return n == 1
 
-        # do it again Bethel
+        #Do it again Bethel
